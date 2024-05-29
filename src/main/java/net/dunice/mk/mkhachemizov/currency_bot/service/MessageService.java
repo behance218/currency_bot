17cb1ffce1eccfe9a56d2cb8a40ed28cbc4b87c2
Mkhachemizov <mkhachemizov218@mail.ru>
2024-05-29 17:53:19 +0300

    feature/1.3 - inline keyboard reworked in reply keyboard (work in progress)

--- src/main/java/net/dunice/mk/mkhachemizov/currency_bot/service/MessageService.java
+++ src/main/java/net/dunice/mk/mkhachemizov/currency_bot/service/MessageService.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
@@ -28,6 +32,34 @@ public class MessageService extends TelegramLongPollingBot {
     private Long lastMessageId = null;
     private Long lastChatId = null;
 
+    @Override
+    public String getBotUsername() {
+        return botProperties.name();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botProperties.token();
+    }
+
+    //TODO: ДОПИЛИТЬ ЭТОТ МЕТОД, ВОЗМОЖНО СНОВА РАЗДЕЛИТЬ MESSAGE SERVICE ОТ TELEGRAMLONGPOLLINGBOT-а, перенести как раньше отдельно
+    @Override
+    public void onUpdateReceived(Update update) {
+//        SendMessage message = messageReceiver(update);
+//        if (message != null) {
+//            try {
+//                execute(message);
+//                if (message.getText().contains("Меню") || message.getReplyMarkup() == getExpandedMenuKeyboard()) {
+//                    lastMessageId = message.getMessageId;
+//                    lastChatId = Long.valueOf(message.getChatId());
+//                }
+//            } catch (TelegramApiException e) {
+//                log.error("При ответе юзеру возникла проблема", e);
+//            }
+////            sendMessageAndSaveId(message);
+//        }
+    }
+
     public SendMessage messageReceiver(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             return handleTextMessage(update);
@@ -39,124 +71,167 @@ public class MessageService extends TelegramLongPollingBot {
     }
 
     private SendMessage handleTextMessage(Update update) {
-        var text = update.getMessage().getText();
-        var chatId = update.getMessage().getChatId();
-        var name = update.getMessage().getChat().getFirstName();
+        String text = update.getMessage().getText();
+        Long chatId = update.getMessage().getChatId();
+        String name = update.getMessage().getChat().getFirstName();
+
+        deletePreviousMessage();
 
         String responseText;
+        ReplyKeyboardMarkup replyMarkup = getMenuKeyboard();
+
         if (text.equals("/start")) {
-            deletePreviousMessage();
             responseText = String.format("Привет, %s! Я - твой телеграм бот-конвертер/актуатор валют", name);
         }
         else if (text.equals("/stop")) {
-            deletePreviousMessage();
             responseText = String.format("До свидания, %s! Рад был помочь!", name);
         }
+        else if (text.equals("Меню")) {
+            responseText = "Выберите действие из меню ниже";
+            replyMarkup = getExpandedMenuKeyboard();
+        }
+        else if (text.equals("Новый курс")) {
+            responseText = String.format("Понял тебя, %s! Загружаю актуальный курс на сегодня", name);
+            Map<String, Double> rates = currencyUpdaterService.updateCurrencies();
+            responseText += "\nТекущие курсы по отношению к рублю:\n" + rates.entrySet().stream()
+                    .map(entry -> entry.getKey() + ": " + entry.getValue())
+                    .collect(Collectors.joining("\n"));
+            replyMarkup = getExpandedMenuKeyboard();
+        }
+        else if (text.equals("Удалить старый курс")) {
+            responseText = String.format("Понял тебя, %s! Удаляю неактуальные данные. Теперь при нажатии на кнопку " +
+                    "получения нового курса вы получите самый свежий курс на сегодняшний день", name);
+            outdatedCurrencyDeleteService.deleteOutdatedCurrency();
+            replyMarkup = getExpandedMenuKeyboard();
+        }
         else {
-            deletePreviousMessage();
             responseText = "Я таких команд не знаю, к сожалению!";
         }
 
-        var message = new SendMessage();
+        SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(responseText);
-        message.setReplyMarkup(getInlineKeyboard());
-        deletePreviousMessage();
+        message.setReplyMarkup(replyMarkup);
+
         return message;
     }
 
     private SendMessage handleCallbackQuery(Update update) {
-        var callbackQuery = update.getCallbackQuery();
-        var data = callbackQuery.getData();
-        var chatId = callbackQuery.getMessage().getChatId();
-        var name = callbackQuery.getMessage().getChat().getFirstName();
+        CallbackQuery callbackQuery = update.getCallbackQuery();
+        String data = callbackQuery.getData();
+        Long chatId = callbackQuery.getMessage().getChatId();
+        String name = callbackQuery.getMessage().getChat().getFirstName();
+
+        deletePreviousMessage();
 
         String responseText;
         switch (data) {
-            case "Старт" ->
-                    responseText = String.format("Привет, %s! Я твой телеграм бот-конвертер/актуатор валют", name);
-            case "Новый курс" -> {
+            case "Старт":
+                responseText = String.format("Привет, %s! Я твой телеграм бот-конвертер/актуатор валют", name);
+                break;
+            case "Новый курс":
                 responseText = String.format("Понял тебя, %s. Загружаю актуальный курс на сегодня", name);
                 Map<String, Double> rates = currencyUpdaterService.updateCurrencies();
                 responseText += "\nТекущие курсы по отношению к рублю:\n" + rates.entrySet().stream()
                         .map(entry -> entry.getKey() + ": " + entry.getValue())
                         .collect(Collectors.joining("\n"));
-            }
-            case "Удалить старый курс" -> {
+                break;
+            case "Удалить старый курс":
                 responseText = String.format("Понял тебя, %s. Удаляю устаревшие данные", name);
-                deletePreviousMessage();
                 outdatedCurrencyDeleteService.deleteOutdatedCurrency();
-            }
-            case "Стоп" -> responseText = String.format("До свидания, %s! Рад был помочь!", name);
-            default -> responseText = "Я таких команд не знаю, к сожалению!";
+                break;
+            case "Стоп":
+                responseText = String.format("До свидания, %s! Рад был помочь!", name);
+                break;
+            default:
+                responseText = "Я таких команд не знаю, к сожалению!";
+                break;
         }
 
-        var message = new SendMessage();
+        SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(responseText);
-//        deletePreviousMessage();
+        message.setReplyMarkup(getExpandedMenuKeyboard());
 
         return message;
     }
 
-    private InlineKeyboardMarkup getInlineKeyboard() {
-        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
 
-        List<InlineKeyboardButton> row1 = new ArrayList<>();
-        row1.add(createButton("Новый курс", "Новый курс"));
-        row1.add(createButton("Стоп", "stop"));
-        row1.add(createButton("Удалить старый курс", "Удалить старый курс"));
+    private ReplyKeyboardMarkup getMenuKeyboard() {
+        KeyboardRow row = new KeyboardRow();
+        row.add("Меню");
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row);
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setKeyboard(keyboard);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+
+        return keyboardMarkup;
+    }
+
+    private ReplyKeyboardMarkup getExpandedMenuKeyboard() {
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Новый курс");
+        row1.add("Удалить старый курс");
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
         keyboard.add(row1);
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        inlineKeyboardMarkup.setKeyboard(keyboard);
-        return inlineKeyboardMarkup;
+
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setKeyboard(keyboard);
+        keyboardMarkup.setResizeKeyboard(true);
+        keyboardMarkup.setOneTimeKeyboard(false);
+
+        return keyboardMarkup;
     }
 
-    private InlineKeyboardButton createButton(String text, String callbackData) {
-        InlineKeyboardButton button = new InlineKeyboardButton();
-        button.setText(text);
-        button.setCallbackData(callbackData);
-        return button;
-    }
+//    private InlineKeyboardMarkup getInlineKeyboard() {
+//        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+//
+//        List<InlineKeyboardButton> row1 = new ArrayList<>();
+//        row1.add(createButton("Новый курс", "Новый курс"));
+//        row1.add(createButton("Стоп", "stop"));
+//        row1.add(createButton("Удалить старый курс", "Удалить старый курс"));
+//        keyboard.add(row1);
+//        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+//        inlineKeyboardMarkup.setKeyboard(keyboard);
+//        return inlineKeyboardMarkup;
+//    }
+
+//    private InlineKeyboardButton createButton(String text, String callbackData) {
+//        InlineKeyboardButton button = new InlineKeyboardButton();
+//        button.setText(text);
+//        button.setCallbackData(callbackData);
+//        return button;
+//    }
 
     private void deletePreviousMessage() {
         if (lastMessageId != null && lastChatId != null) {
-            deleteMessage(lastChatId, lastMessageId);
+            DeleteMessage deleteMessage = new DeleteMessage();
+            deleteMessage.setChatId(lastChatId.toString());
+            deleteMessage.setMessageId(lastMessageId.intValue());
+            try {
+                execute(deleteMessage);
+            }
+            catch (TelegramApiException e) {
+                log.error("Не удалось удалить предыдущее сообщение", e);
+            }
             lastMessageId = null;
             lastChatId = null;
         }
     }
 
-    @Override
-    public String getBotUsername() {
-        return botProperties.name();
-    }
-
-    @Override
-    public String getBotToken() {
-        return botProperties.token();
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        SendMessage message = messageReceiver(update);
+    private void sendMessageAndSaveId(SendMessage message) {
         try {
-            execute(message);
+            Message sentMessage = execute(message);
+            lastMessageId = Long.valueOf(sentMessage.getMessageId());
+            lastChatId = sentMessage.getChatId();
         }
         catch (TelegramApiException e) {
-            log.error("При ответе юзеру возникла проблема", e);
-        }
-    }
-
-    public void deleteMessage(Long chatId, Long messageId) {
-        DeleteMessage deleteMessage = new DeleteMessage();
-        deleteMessage.setChatId(chatId);
-        deleteMessage.setMessageId(messageId.intValue());
-        try {
-            execute(deleteMessage);
-        }
-        catch (TelegramApiException e) {
-            log.error("Не удалось удалить сообщение", e);
+            log.error("Не удалось отправить сообщение или получить ID", e);
         }
     }
 }
