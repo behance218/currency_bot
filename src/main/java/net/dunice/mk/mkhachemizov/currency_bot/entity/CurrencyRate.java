65e68f35be3034d0390a3e4e4a83d1368bf426a0
Mkhachemizov <mkhachemizov218@mail.ru>
2024-05-30 12:51:24 +0300

    feature/1.4 - reply keyboard finish + delete messages deprecated mark

--- src/main/java/net/dunice/mk/mkhachemizov/currency_bot/entity/CurrencyRate.java
+++ src/main/java/net/dunice/mk/mkhachemizov/currency_bot/entity/CurrencyRate.java
@@ -1,11 +1,12 @@
 package net.dunice.mk.mkhachemizov.currency_bot.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 
 @Entity
 @Getter
--- src/main/java/net/dunice/mk/mkhachemizov/currency_bot/service/OutdatedCurrencyDeleteScheduler.java
+++ src/main/java/net/dunice/mk/mkhachemizov/currency_bot/service/OutdatedCurrencyDeleteScheduler.java
@@ -1,6 +1,7 @@
 package net.dunice.mk.mkhachemizov.currency_bot.service;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
